28826bf79495e44239e791f4f754e81f5ee6b81c
Md Shahbaz Alam <shahbazalam75508@gmail.com>
2023-01-30 13:31:34 +0530

    Update PORT to work on all environments

--- server/src/filterTwitterSpaces.js
+++ server/src/filterTwitterSpaces.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const filterTwitterSpaces = require("./routes/filter");
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use("/", filterTwitterSpaces);
 
 // eslint-disable-next-line no-console
-app.listen(8080, () => console.log(`Server started listening at port 8080`));
+app.listen(PORT, () => console.log(`Server started listening at port ${PORT}`));
